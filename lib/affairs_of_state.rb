f20bf4b2e885b9e79bdb9f3dcdc2987f5412b57e
Kevin McPhillips <github@kevinmcphillips.ca>
2016-07-05 18:05:38 -0400

    Syntax cleanup

--- lib/affairs_of_state.rb
+++ lib/affairs_of_state.rb
@@ -8,22 +8,23 @@ module AffairsOfState
 
   module ClassMethods
     def affairs_of_state(*args)
-      @_status_options = ({:column => :status, :allow_blank => false, :scopes => true, :if => nil}).merge(args.extract_options!)
+      @_status_options = { column: :status, allow_blank: false, scopes: true, if: nil }.merge(args.extract_options!)
       @_statuses = args.flatten.map(&:to_s)
 
       const_set("STATUSES", @_statuses)
 
-      validates(@_status_options[:column], :inclusion => {:in => @_statuses, :allow_blank => @_status_options[:allow_blank]}, :if => @_status_options[:if])
+      validates(@_status_options[:column], inclusion: { in: @_statuses, allow_blank: @_status_options[:allow_blank] }, if: @_status_options[:if])
 
       if @_status_options[:scopes]
         @_statuses.each do |status|
-          raise "Affairs of State: '#{ status }' is not a valid status" unless valid_status?(status)
+          raise "Affairs of State: '#{  status }' is not a valid status" unless valid_status?(status)
           self.scope status.to_sym, -> { where(@_status_options[:column] => status.to_s) }
         end
       end
 
       include InstanceMethods
       extend SingletonMethods
+
       true
     end
 
@@ -36,16 +37,16 @@ module AffairsOfState
 
   module InstanceMethods
     def method_missing(method, *args)
-      if self.class::STATUSES.map{|s| "#{s}?".to_sym }.include?(method)
+      if self.class::STATUSES.map{ |s| "#{ s }?".to_sym }.include?(method)
         self.class.send(:define_method, method) do
           self.status == method.to_s.gsub(/\?$/, "")
         end
 
         send method
 
-      elsif self.class::STATUSES.map{|s| "#{s}!".to_sym }.include?(method)
+      elsif self.class::STATUSES.map{ |s| "#{ s }!".to_sym }.include?(method)
         self.class.send(:define_method, method) do
-          self.send("#{self.class.instance_variable_get('@_status_options')[:column]}=", method.to_s.gsub(/\!$/, ""))
+          self.send("#{ self.class.instance_variable_get('@_status_options')[:column] }=", method.to_s.gsub(/\!$/, ""))
           self.save
         end
 
@@ -58,7 +59,7 @@ module AffairsOfState
 
   module SingletonMethods
     def statuses_for_select
-      @_statuses.map{|s| [s.humanize, s]}
+      @_statuses.map{ |s| [s.humanize, s] }
     end
   end
 
