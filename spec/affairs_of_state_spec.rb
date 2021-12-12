c965702a6581694f4202c49587ab2281af5b5358
Kevin McPhillips <github@kevinmcphillips.ca>
2021-12-12 16:09:12 -0500

    Tidy up tests

--- spec/affairs_of_state_spec.rb
+++ spec/affairs_of_state_spec.rb
@@ -7,61 +7,63 @@ describe AffairsOfState do
       affairs_of_state :active, :inactive, :cancelled
     end
 
+    subject { Pie }
+
     it "should validate the column is set" do
-      expect(Pie.new(status: nil)).to_not be_valid
+      expect(subject.new(status: nil)).to_not be_valid
     end
 
     it "should validate that we're not setting it to something stupid" do
-      expect(Pie.new(status: "delicious_pie")).to_not be_valid
+      expect(subject.new(status: "delicious_pie")).to_not be_valid
     end
 
     describe "boolean methods" do
       it "should find the set status" do
-        p = Pie.new status: "active"
+        p = subject.new status: "active"
         expect(p.active?).to be_truthy
       end
 
       it "should not find if a different status is set" do
-        p = Pie.new status: "inactive"
+        p = subject.new status: "inactive"
         expect(p.cancelled?).to be_falsy
       end
     end
 
     describe "update methods" do
       it "should set the value" do
-        p = Pie.create! status: "active"
+        p = subject.create! status: "active"
         expect(p.inactive!).to be_truthy
         expect(p.status).to eq("inactive")
       end
     end
 
     it "should have the statuses method with the nil default" do
-      expect(Pie.statuses).to eq(["active", "inactive", "cancelled"])
+      expect(subject.statuses).to eq(["active", "inactive", "cancelled"])
     end
 
     it "should have the statuses method" do
-      expect(Pie.statuses(:status)).to eq(["active", "inactive", "cancelled"])
+      expect(subject.statuses(:status)).to eq(["active", "inactive", "cancelled"])
     end
 
     it "should provide a method to pass to dropdowns" do
-      expect(Pie.statuses_for_select).to eq([["Active", "active"], ["Inactive", "inactive"], ["Cancelled", "cancelled"]])
+      expect(subject.statuses_for_select).to eq([["Active", "active"], ["Inactive", "inactive"], ["Cancelled", "cancelled"]])
     end
 
     describe "scopes" do
       it "should have a finder to match the status name" do
-        Pie.create! status: "active"
-        Pie.create! status: "inactive"
-        Pie.create! status: "active"
-        Pie.create! status: "cancelled"
+        subject.create! status: "active"
+        subject.create! status: "inactive"
+        subject.create! status: "active"
+        subject.create! status: "cancelled"
 
-        expect(Pie.active.size).to eq(2)
-        expect(Pie.inactive.size).to eq(1)
-        expect(Pie.cancelled.size).to eq(1)
+        expect(subject.active.size).to eq(2)
+        expect(subject.inactive.size).to eq(1)
+        expect(subject.cancelled.size).to eq(1)
       end
     end
 
     after(:each) do
-      Pie.destroy_all
+      subject.destroy_all
     end
   end
 
@@ -72,16 +74,18 @@ describe AffairsOfState do
       affairs_of_state :active, :inactive, column: :super_status
     end
 
+    subject { Pie2 }
+
     it "should validate the column is set" do
-      expect(Pie2.new(status: nil, super_status: "active")).to be_valid
+      expect(subject.new(status: nil, super_status: "active")).to be_valid
     end
 
     it "should know the accessors" do
-      expect(Pie2.new(status: nil, super_status: "inactive").inactive?).to be(true)
+      expect(subject.new(status: nil, super_status: "inactive").inactive?).to be(true)
     end
 
     it "should know the setters" do
-      instance = Pie2.create!(status: nil, super_status: "inactive")
+      instance = subject.create!(status: nil, super_status: "inactive")
       expect(instance.active!).to be(true)
       expect(instance.super_status).to eq("active")
     end
@@ -94,8 +98,10 @@ describe AffairsOfState do
       affairs_of_state :active, :inactive, allow_blank: true
     end
 
+    subject { Pie3 }
+
     it "should validate the column is set" do
-      expect(Pie3.new(status: nil)).to be_valid
+      expect(subject.new(status: nil)).to be_valid
     end
   end
 
@@ -106,8 +112,10 @@ describe AffairsOfState do
       affairs_of_state [:on, :off]
     end
 
+    subject { Pie4 }
+
     it "should work too if that's what floats your boat" do
-      expect(Pie4.statuses).to eq(["on", "off"])
+      expect(subject.statuses).to eq(["on", "off"])
     end
   end
 
@@ -118,8 +126,10 @@ describe AffairsOfState do
       affairs_of_state :active, :inactive, scopes: false
     end
 
-    it "should work too if that's what floats your boat" do
-      expect(Pie5).to_not respond_to(:active)
+    subject { Pie5 }
+
+    it "should not respond if the scopes are not needed" do
+      expect(subject).to_not respond_to(:active)
     end
   end
 
@@ -132,15 +142,17 @@ describe AffairsOfState do
       attr_accessor :is_going_to_validate
     end
 
+    subject { Pie6 }
+
     it "should enforce the validation if the :if param is true" do
-      p = Pie6.new
+      p = subject.new
       p.is_going_to_validate = true
       p.status = "pie"
       expect(p).to_not be_valid
     end
 
     it "should not enforce the validation if the :if param evaluates to false" do
-      p = Pie6.new
+      p = subject.new
       p.is_going_to_validate = false
       p.status = "pie"
       expect(p).to be_valid
@@ -160,15 +172,17 @@ describe AffairsOfState do
       end
     end
 
+    subject { Pie7 }
+
     it "should enforce the validation if the :if param is true" do
-      p = Pie7.new
+      p = subject.new
       p.is_going_to_validate = true
       p.status = "pie"
       expect(p).to_not be_valid
     end
 
     it "should not enforce the validation if the :if param evaluates to false" do
-      p = Pie7.new
+      p = subject.new
       p.is_going_to_validate = false
       p.status = "pie"
       expect(p).to be_valid
@@ -189,8 +203,10 @@ describe AffairsOfState do
       affairs_of_state :moderated, :unmoderated, column: :super_status
     end
 
+    subject { Pie9 }
+
     it "declares two status columns" do
-      p = Pie9.new
+      p = subject.new
       p.inactive!
       p.unmoderated!
       expect(p).to be_inactive
@@ -221,24 +237,24 @@ describe AffairsOfState do
 
     it "raises if statuses is called with no argument" do
       expect {
-        Pie9.statuses
+        subject.statuses
       }.to raise_error(ArgumentError)
     end
 
     it "returns the statuses for the column" do
-      expect(Pie9.statuses(:status)).to eq(["active", "inactive"])
-      expect(Pie9.statuses("super_status")).to eq(["moderated", "unmoderated"])
+      expect(subject.statuses(:status)).to eq(["active", "inactive"])
+      expect(subject.statuses("super_status")).to eq(["moderated", "unmoderated"])
     end
 
     it "raises if statuses_for_select is called with no argument" do
       expect {
-        Pie9.statuses_for_select
+        subject.statuses_for_select
       }.to raise_error(ArgumentError)
     end
 
     it "returns the statuses_for_select for the column" do
-      expect(Pie9.statuses_for_select(:status)).to eq([["Active", "active"], ["Inactive", "inactive"]])
-      expect(Pie9.statuses_for_select("super_status")).to eq([["Moderated", "moderated"], ["Unmoderated", "unmoderated"]])
+      expect(subject.statuses_for_select(:status)).to eq([["Active", "active"], ["Inactive", "inactive"]])
+      expect(subject.statuses_for_select("super_status")).to eq([["Moderated", "moderated"], ["Unmoderated", "unmoderated"]])
     end
   end
 end
